3d11fb70558cb949761bc63d27dc8d9c9d22b5d7
Nichil <hementhnichilg@gmail.com>
2025-08-05 23:56:19 +0530

    updated server.js
    simple express server

--- server.js
+++ server.js
@@ -1,9 +1,25 @@
 import express from 'express'
 
+const celeb_data = {
+    type:"movie_hero",
+    name:"brad pitt"
+}
+
 const PORT = 8000
 
 const app = express()
 
+//first param is the path
+//=> only do the callback fn is a get request is passed to this path - currently '/'
+//callback function takes in the req object and the response object
+//req and res objects give us access to the request-response cycle.
+app.get('/',(req,res)=> {
+    res.json(celeb_data)
+    //http can only send strings
+    //.json method on the response object will convert js-object(or any other datatype)
+    // to strings for http transfer
+})
+
 app.listen(PORT, ()=>{
-    console.log("this express server is running")
+    console.log(`this express server is runnning on port ${PORT}`)
 })
